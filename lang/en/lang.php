e34994dfb23184d18f22071d3301e5733bcd6c46
Matteo Trubini <7964032+matteotrubini@users.noreply.github.com>
2023-08-07 16:31:03 +0200

    Add endpoint field (#2)

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -10,4 +10,6 @@ return [
     'mailgun_domain_comment' => 'Please specify the Mailgun domain name.',
     'mailgun_secret' => 'Mailgun secret',
     'mailgun_secret_comment' => 'Enter your Mailgun API key.',
+    'mailgun_endpoint' => 'Mailgun endpoint',
+    'mailgun_endpoint_comment' => 'Enter your Mailgun API Endpoint.',
 ];
