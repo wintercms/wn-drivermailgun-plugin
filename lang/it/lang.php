19cac18f292bd2f710596a8a6fdccf4d23e5ec1c
Matteo Trubini <7964032+matteotrubini@users.noreply.github.com>
2026-01-21 17:23:39 +0100

    Improve Italian translation (#4)

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
+    'plugin' => [
+        'name' => 'Mail Driver (Mailgun)',
+        'description' => 'Driver che aggiunge il supporto per il mailer di Mailgun a WinterCMS',
+    ],
+
     'mailgun_domain' => 'Dominio Mailgun',
-    'mailgun_domain_comment' => 'Inserisci il nome dominio Mailgun.',
-    'mailgun_secret' => 'Chiave Mailgun',
-    'mailgun_secret_comment' => 'Inserisci la tua chiave per l\'utilizzo delle API Mailgun.',
+    'mailgun_domain_comment' => 'Specifica il nome del dominio attivo su Mailgun.',
+    'mailgun_secret' => 'Chiave API Mailgun',
+    'mailgun_secret_comment' => 'Inserisci la tua chiave API di Mailgun.',
+    'mailgun_endpoint' => 'Endpoint API Mailgun',
+    'mailgun_endpoint_comment' => 'Inserisci il tuo Endpoint API di Mailgun.',
 ];
